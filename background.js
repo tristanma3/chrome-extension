56e255f123c28266a2243cd2fb4a49fa424200be
Tristan Ma <tristanma3@gmail.com>
2021-08-01 12:12:08 -0700

    adding background tab tracking

--- background.js
+++ background.js
@@ -1,9 +1,23 @@
-chrome.tabs.onCreated.addListener(function (tab) {
-  const now = new Date();
-  tab.url;
-  chrome.storage.local.set({ data: d });
+console.log("hellO");
+chrome.storage.local.clear();
 
-  // document.getElementById("text").innerText = new Date(
-  //     performance.timing.navigationStart
-  //   ).toString();
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (!changeInfo.url) {
+    return;
+  }
+  chrome.storage.local.get("dates", function (result) {
+    const now = { url: tab.url, time: new Date() };
+    if (!result.dates) {
+      result.dates = [];
+    }
+    result.dates.push(now);
+    result.dates.forEach(console.log);
+    chrome.storage.local.set(result);
+  });
+  //console.log(tab);
+
+  //d3 library
+  //var gettingAll = browser.windows.getAll(
+  //getInfo                // optional object
+  //)
 });
